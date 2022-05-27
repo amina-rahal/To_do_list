02fd64b05191fdb5536ffd34e0ad19a0989a1785
Amina <aminarahal39@gmail.com>
2022-05-27 15:33:35 +0100

    filter tasks

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ function loadEventListeners()
 {
     // Add task event
     form.addEventListener('submit', addTask);
+    // remove task event
+    taskList.addEventListener('click', removeTask);
+    // clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks events
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -46,4 +52,43 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// remove Task
+function removeTask(e)
+{
+    if (e.target.parentElement.classList.contains('delete-item'))
+    {
+        if (confirm('Are you sure?'))
+        {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// clear Tasks
+function clearTasks()
+{
+    taskList.innerHTML = '';
+}
+
+// Filter tasks
+function filterTasks(e)
+{
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(
+        function(task)
+        {
+            const item = task.firstChild.textContent;
+            if (item.toLowerCase().indexOf(text)!= -1)
+            {
+                task.style.display = 'block';
+            }
+            else
+            {
+                task.style.display = 'none';
+            }
+        }
+    );
 }
